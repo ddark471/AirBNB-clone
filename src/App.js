76984635fee09531410a258f5874a0b5010281d5
Dodajon Khusnitdinov <ddarkk471@gmail.com>
2023-01-10 20:05:12 +0300

    nav-bar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return ();
-}
+  return (
+    <div className="wrap">
+        <nav className="wrap__topbar">
+          <div className="topbar__rectangle">
+            <img src="./airbnb-logo.svg" alt="airbnb logo" className="rectangle__logo"/>
+          </div>
+          <div className="topbar__main">
+            <img src="./image 28.svg" alt="" className="main__image1" />
+            
+            <div className="main__image2">
+              <img src="./image 30.svg" alt="image2" className="image2__1"/>
+              <img src="./image 26.svg" alt="iamge3" className="image2__2"/>
+            </div>
+
+            <div className="main__image3">
+              <img src="./image 27.svg" alt="image4" className="image3__1"/>
+              <img src="./image 29.svg" alt="iamge5" className="image3__2"/>
+            </div>
+
+            <div className="main__image4">
+              <img src="./image 22.svg" alt="image6" className="image4__1"/>
+              <img src="./image 23.svg" alt="image7" className="image4__2"/>
+            </div>
+            
+            <div className="main__image5">
+              <img src="./image 25.svg" alt="image8" className="image5__1"/>
+              <img src="./image 24.svg" alt="iamge9" className="image5__2"/>
+            </div>
+          </div>
+        </nav>  
+    </div>
+  )
+};
 
 export default App;
